de36a82e17d8a63c61c7ba61183cc4673bdb1ef6
Carlos Andre Couto <carlos.couto@external.cnhind.com>
2017-10-06 15:57:33 -0300

    Ajuste modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,8 @@ import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 
 import { ContatoModule } from './contato/contato.module';
 import { QuemSomosModule } from './quem-somos/quem-somos.module';
@@ -17,13 +17,10 @@ import { NossaVisaoModule } from './nossa-visao/nossa-visao.module';
 		BrowserModule,
 		FormsModule,
 		HttpModule,
-		RouterModule,
 		ContatoModule,
 		QuemSomosModule,
 		NossaVisaoModule,
-		RouterModule.forRoot([
-
-		])
+		AppRoutingModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
