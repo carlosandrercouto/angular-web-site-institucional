63cdbac67e339dbd54ff97c7280f5ce648d8b0a0
Carlos Andre Couto <carlos.couto@external.cnhind.com>
2017-10-09 16:27:57 -0300

    Adicao rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ContatoComponent } from './contato/contato.component';
 
 @NgModule({
 	imports: [
 		RouterModule.forRoot([
-
+			{path: 'contato', component: ContatoComponent}
 		])
 	],
 	exports: [
--- src/app/nossa-visao/nossa-visao.module.ts
+++ src/app/nossa-visao/nossa-visao.module.ts
@@ -1,16 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NossaVisaoRoutingModule } from './nossa-visao-routing.module';
 import { NossaVisaoComponent } from './nossa-visao.component';
 
 @NgModule({
 	imports: [
 		CommonModule,
-		RouterModule.forChild([
-			{
-				path: 'nossa-visao', component: NossaVisaoComponent
-			}
-		])
+		NossaVisaoRoutingModule
 	],
 	declarations: [
 		NossaVisaoComponent
